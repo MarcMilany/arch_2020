c2a20c24bee2a62e3d14117a82a3bbd9fa81df0f
MarcMilany <alecs1874@gmail.com>
2020-05-09 18:48:54 +0300

    rellease v1

---
+++ archmy1.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+# Arch Linux Fast Install - Быстрая установка Arch Linux
+# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (конфиг XFCE, темы, программы и т.д.).
+
+# В разработке принимали участие:
+# Алексей Бойко https://vk.com/ordanax
+# Степан Скрябин https://vk.com/zurg3
+# Михаил Сарвилин https://vk.com/michael170707
+# Данил Антошкин https://vk.com/danil.antoshkin
+
+# Команды по установке :
+# archiso login: root (automatic login)
+# Терминальный Мануал Arch Wiki :
+# Мы можем просмотреть какие файлы сейчас находятся катологе пользователя root ls -la
+# ls -la
+# В пользовательском катологе пользователя root есть файл install.txt, если его открыть мы увидем тот же мануал 
+# Arch Wiki, как и на официальном сайте Arch'a .
+# less install.txt
+
+# Этот мануал можно не закрывать, если у вас нет распечатанного Wiki по установке, или планшета, или второго компьютера, где можно посмотреть в шпаргалку , то нужно нажать Alt + F2 и откроется вторая консоль установки .
+# Сейчас мы находимся в первой консоле Alt + F1 , и можем переключаться между консолями командами Alt + F* , где + F* нужная вам консоль . # Всего вроде там 6 консолей (менеджеров интепретации).
+# Если во второй консоли запросит логин (login) просто введите - root , пример : "archiso login : root ".
+# Перемещаться и читать в мануале можно с помощью стрелочек (вверх или вниз) , допустим Alt + F1 читать мануал , а Alt + F2 продолжаем установку Arch'a . 
+
+# Installation guide - Arch Wiki
+# https://wiki.archlinux.org/index.php/Installation_guide
+
+# Важно !!! 
+# При установке системы Arch Linux наличие подключения к интернету обязательно.
+# Служба DHCP уже запущена при загрузке для найденных Ethernet-адаптеров. 
+# Для беспроводных сетевых адаптеров запустите wifi-menu. 
+# Если необходимо настроить статический IP или использовать другие средства настройки сети, остановите службу DHCP командой systemctl stop dhcpcd.service и используйте netctl.
+# Подключаем интернет (пункт для тех у кого автоматически не подключился интернет):
+# Если у вас проводной интернет - dhcpcd  проводное соединение подхватится автоматом
+# Если выпадает ошибка с номером 213 или др.,то выполните следующие команды:
+# kill 213 и вновь запускаем dhcpcd
+
+# Если вы подключаетесь к интернет по WiFi, то нужно подключиться к вашей WiFi-сети. 
+# Я не пробовал устанавливать ArchLinux на компьютере с WiFi, но приведу выдержки из руководства (отпишитесь, пожалуйста, в комментариях, работает ли этот способ). 
+# Сначала необходимо определить название WiFi интерфейса, для этого выполняем команду:
+# iwconfig
+# Затем воспользуемся утилитой wifi-menu: 
+# wifi-menu имя_интерфейса
+# wifi-menu
+
+# Если в результате выходит ошибка о не существовании wlan0, то узнайте как 
+# называется ваш сетевой интерфейс с помощью iwconfig и введите wifi-menu <интерфейс>
+# Статья по настройке Wi-fi - wifi-menu:
+# http://rus-linux.net/MyLDP/consol/setup-wifi-in-command-line.html
+
+# Подключение через PPPoE: 
+# используйте для настройки программу pppoe-setup, для запуска — pppoe-start
+
+# Проверим получены ли сетевые настройки:
+# ipddr
+
+# Посмотрим наши DNC сервера:
+# cat /etc/resolv.conf
+
+# Подключаем wifi
+# wifi-menu
+# Или если у вас проводной интернет, то подключаем службы DHCP
+# dhcpcd
+# ping -c 5 8.8.8.8
+# ping -c 5 ya.ru
+# ping -c 2 archlinux.org
+# ping -c 5 google.com
+
+echo 'Для проверки интернета можно пропинговать какой-либо сервис'
+ping -c2 archlinux.org
+
+echo 'Настроим русский язык, изменим консольный шрифт на тот, который поддерживает кириллицу для удобства работы'
+loadkeys ru
+setfont cyr-sun16
+echo 'Скрипт сделан на основе чеклиста Бойко Алексея по Установке ArchLinux'
+echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
+
+echo '2.3 Синхронизация системных часов'
+timedatectl set-ntp true
+
+echo '2.4 создание разделов'
+(
+  echo o;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +2G;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +8G;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +35G;
+
+  echo n;
+  echo p;
+  echo;
+  echo;
+  echo a;
+  echo 1;
+
+  echo w;
+) | fdisk /dev/sda
+
+echo 'Ваша разметка диска'
+fdisk -l
+
+echo '2.4.2 Форматирование дисков'
+mkfs.ext2  /dev/sda1 -L boot
+mkswap /dev/sda2 -L swap
+mkfs.ext4  /dev/sda3 -L root
+mkfs.ext4  /dev/sda4 -L home
+
+echo '2.4.3 Монтирование дисков'
+mount /dev/sda3 /mnt
+mkdir /mnt/{boot,home}
+mount /dev/sda1 /mnt/boot
+swapon /dev/sda2
+mount /dev/sda4 /mnt/home
+
+echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
+echo "## Russia" > /etc/pacman.d/mirrorlist
+echo "#Server = http://mirror.rol.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+echo "Server = https://mirror.rol.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+echo "#Server = http://mirror.truenetwork.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+echo "Server = https://mirror.yandex.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+echo "#Server = http://archlinux.zepto.cloud/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+
+echo 'Активируем новые репы (зеркала) от Яндекс' 
+pacman-key --init
+pacman-key --populate archlinux
+pacman -Sy
+
+echo '3.2 Установка основных пакетов'
+pacstrap /mnt base base-devel linux-lts linux-firmware nano dhcpcd netctl vim
+
+echo '3.3 Настройка системы, генерируем fstab'
+genfstab -pU /mnt >> /mnt/etc/fstab
+
+echo 'Прокидываем правильные быстрые репы (зеркала) внутрь'
+cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+
+echo 'Меняем корень и переходим в нашу недавно скачанную систему'
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/archmy2.sh)"
---
+++ archmy2.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+read -p "Введите имя компьютера: " hostname
+read -p "Введите имя пользователя: " username
+
+echo 'Прописываем имя компьютера'
+echo $hostname > /etc/hostname
+ln -svf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+
+echo 'Измените имя хоста '
+echo "127.0.0.1	localhost.(none)" > /etc/hosts
+echo "127.0.1.1	Terminator" >> /etc/hosts
+echo "::1	localhost ip6-localhost ip6-loopback" >> /etc/hosts
+echo "ff02::1 ip6-allnodes" >> /etc/hosts
+echo "ff02::2 ip6-allrouters" >> /etc/hosts
+
+echo '3.4 Добавляем русскую локаль системы'
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
+
+echo 'Обновим текущую локаль системы'
+locale-gen
+
+echo 'Указываем язык системы'
+echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
+
+echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
+echo 'KEYMAP=ru' >> /etc/vconsole.conf
+echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
+echo 'FONT_MAP=' >> /etc/vconsole.conf
+echo 'CONSOLEMAP' >>> /etc/vconsole.conf
+
+echo 'Создадим загрузочный RAM диск'
+mkinitcpio -p linux
+
+echo 'Создаем root пароль'
+passwd
+
+echo '3.5 Устанавливаем загрузчик'
+pacman -Syy
+pacman -S grub --noconfirm 
+grub-install /dev/sda
+
+echo 'Если в системе будут несколько ОС, то это также ставим'
+pacman -S os-prober mtools fus
+
+echo 'Обновляем grub.cfg (Сгенерируем grub.cfg)'
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo 'Ставим программу для Wi-fi'
+pacman -S dialog wpa_supplicant --noconfirm 
+
+echo 'Добавляем пользователя'
+useradd -m -g users -G wheel,audio,games,lp,optical,power,scanner,storage,video,sys -s /bin/bash $username
+
+echo 'Устанавливаем пароль пользователя'
+passwd $username
+
+echo 'Устанавливаем SUDO'
+pacman -S sudo --noconfirm
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
+echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+sed -i 's/#Color/Color/' /etc/pacman.conf
+echo 'ILoveCandy' >> /etc/pacman.conf
+pacman -Syy
+
+echo "Куда устанавливем Arch Linux на виртуальную машину?"
+read -p "1 - Да, 0 - Нет: " vm_setting
+if [[ $vm_setting == 0 ]]; then
+  gui_install="xorg-server xorg-drivers xorg-xinit xorg-server-utils mesa"
+elif [[ $vm_setting == 1 ]]; then
+  gui_install="xorg-server xorg-drivers xorg-xinit virtualbox-guest-utils"
+fi
+
+echo 'Ставим иксы и драйвера'
+pacman -S $gui_install
+
+echo 'Ставим Xfce, LXDM и сеть'
+pacman -S xfce4 xfce4-goodies networkmanager network-manager-applet ppp --noconfirm
+
+echo 'Ставим менеджера входа'
+pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm
+
+echo 'Ставим шрифты'
+pacman -S ttf-liberation ttf-dejavu --noconfirm 
+
+echo 'Ставим Bluetooth and Sound support'
+pacman -S bluez bluez-libs bluez-cups bluez-utils --noconfirm
+pacman -S alsa-utils alsa-plugins alsa-firmware alsa-lib alsa-utils --noconfirm 
+pacman -S pulseaudio pulseaudio-alsa pavucontrol pulseaudio-zeroconf pulseaudio-bluetooth xfce4-pulseaudio-plugin --noconfirm
+
+echo 'Ставим Архиваторы "Compression Tools"'
+pacman -S zip unzip unrar p7zip zlib zziplib --noconfirm
+
+echo 'Ставим Драйвера принтера (Print support)'
+sudo pacman -S cups ghostscript cups-pdf --noconfirm
+
+echo 'Установка базовых программ и пакетов'
+sudo pacman -S firefox firefox-i18n-ru audacious audacious-plugins transmission-gtk transmission-cli gedit gedit-plugins geany geany-plugins bash-completion pidgin ufw onboard iftop htop nmap sane testdisk vlc pv f2fs-tools dosfstools ntfs-3g file-roller gvfs aspell-ru --noconfirm
+
+echo 'Подключаем автозагрузку менеджера входа и интернет'
+systemctl enable lightdm.service NetworkManager
+
+echo 'Установка завершена! Перезагрузите систему.'
+echo 'Если хотите подключить AUR, установить мои конфиги XFCE, тогда после перезагрузки и входа в систему, установите wget (sudo pacman -S wget) и выполните команду:'
+echo 'wget git.io/archmy3.sh && sh archmy3.sh'
+echo 'Выйдем из установленной системы'
+exit
+
+echo 'Далее отмонтируем все ранее монтируемые разделы'
+#При желании можно вручную размонтировать все разделы с помощью umount -R /mnt: это позволяет заметить любые «занятые» разделы и найти причину с помощью
+#umount /mnt/{boot,home,}
+#или
+#umount /mnt/home
+#umount /mnt/boot
+#umount /mnt
+
+umount -R /mnt/boot
+umount -R /mnt/home
+umount -R /mnt
+
+reboot
+
