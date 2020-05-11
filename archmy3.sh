5ba86a774b471366387001394ff1f6fbd2f2868c
MarcMilany <alecs1874@gmail.com>
2020-05-11 15:09:39 +0300

    Update archmy3.sh

--- archmy3.sh
+++ archmy3.sh
@@ -15,15 +15,30 @@ xdg-user-dirs-update
 echo 'Установка Мультимедиа кодеков (multimedia codecs), и утилит'
 sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-bad gst-plugins-ugly flashplugin libdvdcss libdvdread libdvdnav dvd+rw-tools dvdauthor dvgrab cdrdao gst-libav gst-libav --noconfirm
 
-echo 'Установка Мультимедиа утилит'
-sudo pacman -S deadbeef radiotray smplayer smplayer-skins smplayer-themes smtube spotify vlc-tunein-radio easytag --noconfirm
+echo 'Установка Мультимедиа утилит AUR'
+sudo pacman -S deadbeef smplayer smplayer-skins smplayer-themes smtube easytag --noconfirm
+yay -S radiotray spotify vlc-tunein-radio --noconfirm  
 
-echo 'Установка Офиса (LibreOffice still)'
-sudo pacman -S libreoffice libreoffice-still libreoffice-still-ru --noconfirm
-#sudo pacman -S libreoffice libreoffice-fresh-ru --noconfirm
+echo 'Установка Офиса (LibreOffice still, fresh)'
+echo 'Установить LibreOffice still?'
+read -p "1 - Да, 0 - Нет: " prog_set
+if [[ $prog_set == 1 ]]; then
+sudo pacman -S libreoffice-still libreoffice-still-ru --noconfirm
+elif [[ $prog_set == 0 ]]; then
+  echo 'Установка программ пропущена.'
+fi
+
+echo 'Установить LibreOffice fresh?'
+read -p "1 - Да, 0 - Нет: " prog_set
+if [[ $prog_set == 1 ]]; then
+sudo pacman -S libreoffice libreoffice-fresh-ru --noconfirm
+elif [[ $prog_set == 0 ]]; then
+  echo 'Установка программ пропущена.'
+fi
 
 echo 'Установка программ'
-sudo pacman -S wget cmake git curl galculator-gtk2 flashplugin pepper-flash pidgin pidgin-extprefs pidgin-hotkeys firefox ufw qt4 f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio --noconfirm
+sudo pacman -S wget cmake git curl galculator-gtk2 qt4 f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio --noconfirm
+yay -S  pidgin-extprefs --noconfirm
 
 echo 'Форматируем флешки с файловой системой exFAT в Linux'
 sudo pacman -S exfat-utils fuse-exfat --noconfirm 
@@ -31,7 +46,7 @@ sudo pacman -S exfat-utils fuse-exfat --noconfirm
 echo 'Установить рекомендумые программы?'
 read -p "1 - Да, 0 - Нет: " prog_set
 if [[ $prog_set == 1 ]]; then
-  sudo pacman -S obs-studio veracrypt freemind filezilla cherrytree gimp libreoffice libreoffice-fresh-ru kdenlive doublecmd-gtk2 audacity screenfetch vlc qbittorrent gnome-calculator --noconfirm
+  sudo pacman -S obs-studio veracrypt freemind filezilla cherrytree gimp kdenlive doublecmd-gtk2 audacity screenfetch vlc qbittorrent gnome-calculator --noconfirm
   yay -S dropbox flameshot-git obs-linuxbrowser xflux sublime-text-dev hunspell-ru pamac-aur --noconfirm 
 elif [[ $prog_set == 0 ]]; then
   echo 'Установка программ пропущена.'
