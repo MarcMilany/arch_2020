17ea1058952b19dfaffc086be446dea71d45d1df
MarcMilany <alecs1874@gmail.com>
2020-05-11 19:09:48 +0300

    Update archmy3.sh

--- archmy3.sh
+++ archmy3.sh
@@ -60,7 +60,7 @@ sudo pacman -S galculator-gtk2 qt4 --noconfirm
 echo 'Установить рекомендумые программы?'
 read -p "1 - Да, 0 - Нет: " prog_set
 if [[ $prog_set == 1 ]]; then
-  sudo pacman -S gimp kdenlive --noconfirm 
+  sudo pacman -S gimp kdenlive mediainfo-gui --noconfirm 
 elif [[ $prog_set == 0 ]]; then
   echo 'Установка программ пропущена.'
 fi
@@ -68,7 +68,7 @@ fi
 echo 'Установить рекомендумые программы из AUR?'
 read -p "1 - Да, 0 - Нет: " prog_set
 if [[ $prog_set == 1 ]]; then
-yay -S gksu caffeine-ng fsearch-git cherrytree timeshift mocicon pidgin-extprefs multiload-ng-indicator-gtk xfce4-multiload-ng-plugin-gtk2 debtap xflux sublime-text-dev dropbox flameshot-git hunspell-ru --noconfirm
+yay -S gksu caffeine-ng fsearch-git cherrytree timeshift mocicon pidgin-extprefs multiload-ng-indicator-gtk xfce4-multiload-ng-plugin-gtk2 debtap grub-customizer xflux sublime-text-dev dropbox flameshot-git hunspell-ru --noconfirm
 elif [[ $prog_set == 0 ]]; then
   echo 'Установка программ пропущена.'
 fi
