04cc29261b14873123b68ec5c966db4e47382de4
MarcMilany <alecs1874@gmail.com>
2020-05-11 22:43:47 +0300

    Update archmy3.sh

--- archmy3.sh
+++ archmy3.sh
@@ -52,15 +52,15 @@ elif [[ $prog_set == 0 ]]; then
 fi
 
 echo 'Установка Мультимедиа утилит AUR'
-yay -S radiotray spotify vlc-tunein-radio audiobook-git cozy-audiobooks m4baker-git --noconfirm  
+yay -S radiotray spotify vlc-tunein-radio vlc-pause-click-plugin audiobook-git cozy-audiobooks m4baker-git mp3gain easymp3gain-gtk2 myrulib-git --noconfirm  
 
 echo 'Установка программ'
-sudo pacman -S galculator-gtk2 qt4 --noconfirm
+sudo pacman -S galculator-gtk2 --noconfirm
 
 echo 'Установить рекомендумые программы?'
 read -p "1 - Да, 0 - Нет: " prog_set
 if [[ $prog_set == 1 ]]; then
-  sudo pacman -S gimp kdenlive mediainfo-gui steam --noconfirm 
+  sudo pacman -S gimp kdenlive steam --noconfirm 
 elif [[ $prog_set == 0 ]]; then
   echo 'Установка программ пропущена.'
 fi
@@ -68,7 +68,7 @@ fi
 echo 'Установить рекомендумые программы из AUR?'
 read -p "1 - Да, 0 - Нет: " prog_set
 if [[ $prog_set == 1 ]]; then
-yay -S gksu caffeine-ng fsearch-git cherrytree timeshift mocicon pidgin-extprefs multiload-ng-indicator-gtk xfce4-multiload-ng-plugin-gtk2 debtap grub-customizer xflux sublime-text-dev dropbox flameshot-git hunspell-ru --noconfirm
+yay -S gksu debtap caffeine-ng fsearch-git cherrytree timeshift mocicon pidgin-extprefs multiload-ng-indicator-gtk xfce4-multiload-ng-plugin-gtk2 keepass2-plugin-tray-icon mintstick woeusb-git gconf-editor unetbootin masterpdfeditor font-manager sublime-text-dev webtorrent-desktop skypeforlinux-stable-bin skype-call-recorder vk-messenger viber megasync thunar-megasync yandex-disk yandex-disk-indicator unoconv qt4 dropbox xflux flameshot-git hunspell-ru --noconfirm
 elif [[ $prog_set == 0 ]]; then
   echo 'Установка программ пропущена.'
 fi
