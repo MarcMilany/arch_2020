7dcca02293e4190209d1b395e1cc4c63edfb5a14
MarcMilany <alecs1874@gmail.com>
2020-05-16 23:50:58 +0300

    Update archmy2.sh

--- archmy2.sh
+++ archmy2.sh
@@ -235,7 +235,7 @@ echo -e "${BLUE}:: ${NC}Изменяем имя хоста"
 #echo 'Изменяем имя хоста'
 # Changing the host name
 echo "127.0.0.1	localhost.(none)" > /etc/hosts
-echo "127.0.1.1	Terminator" >> /etc/hosts
+echo "127.0.1.1	$hostname" >> /etc/hosts
 echo "::1	localhost ip6-localhost ip6-loopback" >> /etc/hosts
 echo "ff02::1 ip6-allnodes" >> /etc/hosts
 echo "ff02::2 ip6-allrouters" >> /etc/hosts
