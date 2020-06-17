a16d1a9420e20f7115c7f983f1c666bb87ec4241
MarcMilany <alecs1874@gmail.com>
2020-06-17 16:33:57 +0300

    Update archmy2.sh

--- archmy2.sh
+++ archmy2.sh
@@ -425,7 +425,7 @@ echo -e "${BLUE}:: ${NC}Добавляем пользователя и проп�
 #echo 'Добавляем пользователя и прописываем права, группы'
 # Adding a user and prescribing rights, groups
 #useradd -m -g users -G wheel -s /bin/bash $username
-useradd -m -g users -G wheel,adm,audio,games,lp,optical,power,scanner,storage,video,sys,rfkill -s /bin/bash $username
+useradd -m -g users -G adm,audio,games,lp,optical,power,scanner,storage,video,rfkill,sys,wheel -s /bin/bash $username
 # или есть команда с правами 'админа' :
 #useradd -m -g users -G adm,audio,games,lp,optical,power,scanner,storage,video,sys,rfkill,wheel -s /bin/bash $username
 
