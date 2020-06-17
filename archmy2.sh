f375bc7d7ea77cdee5770ab7d256ca89bc42b090
MarcMilany <alecs1874@gmail.com>
2020-06-17 14:40:08 +0300

    Update archmy2.sh

--- archmy2.sh
+++ archmy2.sh
@@ -411,12 +411,12 @@ pacman -S os-prober mtools fuse
 echo -e "${BLUE}:: ${NC}Ставим программу для Wi-fi"
 #echo 'Ставим программу для Wi-fi'
 # Install the program for Wi-fi
-pacman -S dialog wpa_supplicant iw wireless_tools net-tools --noconfirm 
+pacman -S dialog wpa_supplicant iw wireless_tools net-tools rfkill --noconfirm 
  
 #read -p "Установить программу (пакет) для Wi-fi?: 1 - да 2 - нет " wifi
 #if [[ $wifi == 1 ]]; then
 #  echo 'Ставим программу для Wi-fi'
-#  pacman -S dialog wpa_supplicant iw wireless_tools net-tools --noconfirm 
+#  pacman -S dialog wpa_supplicant iw wireless_tools net-tools rfkill --noconfirm 
 #elif [[ $wifi == 2 ]]; then
 #  echo 'лан'
 #fi
