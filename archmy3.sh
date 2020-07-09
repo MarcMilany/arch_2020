a0c6ef48fe4602a922df45d9a92a29205fe71553
MarcMilany <alecs1874@gmail.com>
2020-07-09 23:15:46 +0300

    Update archmy3.sh

--- archmy3.sh
+++ archmy3.sh
@@ -196,6 +196,129 @@ echo -e "${GREEN}=> ${NC}Для проверки интернета можно �
 # To check the Internet, you can ping a service
 ping -c2 archlinux.org
 
+# ============================================================================
+
+echo -e "${BLUE}:: ${NC}Сделайте резервную копию файла /etc/pacman.d/mirrorlist"
+#echo 'Сделайте резервную копию файла /etc/pacman.d/mirrorlist'
+# Make a backup copy of the file /etc/pacman.d/mirrorlist
+sudo cp -vf /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+# Сохраняем старый список зеркал в качестве резервной копии:
+#mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
+# Переименовываем новый список:
+#mv /etc/pacman.d/mirrorlist.pacnew /etc/pacman.d/mirrorlist
+#mv -f ~/mirrorlist /etc/pacman.d/mirrorlist
+
+#echo -e "${BLUE}:: ${NC}Удалите файл /etc/pacman.d/mirrorlist"
+#echo 'Удалите файл /etc/pacman.d/mirrorlist'
+# Delete files /etc/pacman.d/mirrorlist
+#rm -rf /etc/pacman.d/mirrorlist
+# Удаления старой резервной копии (если она есть, если нет, то пропустите этот шаг):
+#rm /etc/pacman.d/mirrorlist.old
+
+
+echo -e "${BLUE}:: ${NC}3.1 Выбор серверов-зеркал для загрузки. Ставим зеркало от Яндекс"
+#echo '3.1 Выбор серверов-зеркал для загрузки. Ставим зеркало от Яндекс'
+# The choice of mirror sites to download. Putting a mirror from Yandex
+#echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+> /etc/pacman.d/mirrorlist
+cat <<EOF >>/etc/pacman.d/mirrorlist
+
+##
+## Arch Linux repository mirrorlist
+## Generated on 2020-07-03
+## HTTP IPv4 HTTPS
+##
+
+## Russia
+Server = https://mirror.rol.ru/archlinux/\$repo/os/\$arch
+Server = https://mirror.yandex.ru/archlinux/\$repo/os/\$arch
+#Server = http://mirror.rol.ru/archlinux/\$repo/os/\$arch
+#Server = http://mirror.truenetwork.ru/archlinux/\$repo/os/\$arch
+#Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch
+#Server = http://archlinux.zepto.cloud/\$repo/os/\$arch
+
+##
+## Arch Linux repository mirrorlist
+## Generated on 2020-07-03
+## HTTP IPv6 HTTPS
+##
+
+## Russia
+#Server = http://mirror.yandex.ru/archlinux/$repo/os/\$arch
+#Server = https://mirror.yandex.ru/archlinux/$repo/os/\$arch
+#Server = http://archlinux.zepto.cloud/$repo/os/\$arch
+
+EOF
+
+# ---------------------------------------------------------------------------
+
+echo -e "${BLUE}:: ${NC}Посмотреть список серверов-зеркал для загрузки в mirrorlist"
+#echo 'Посмотреть список серверов-зеркал для загрузки в mirrorlist'
+# View the list of mirror servers to upload to mirrorlist
+cat /etc/pacman.d/mirrorlist
+
+# Pacman Mirrorlist Generator
+# https://www.archlinux.org/mirrorlist/
+# Эта страница генерирует самый последний список зеркал, возможный для Arch Linux. Используемые здесь данные поступают непосредственно из внутренней базы данных зеркал разработчиков, используемой для отслеживания доступности и уровня зеркалирования. 
+# Есть два основных варианта: получить список зеркал с каждым доступным зеркалом или получить список зеркал, адаптированный к вашей географии.
+
+# ============================================================================
+#echo -e "${BLUE}:: ${NC}Загрузка свежего списка зеркал со страницы Mirror Status, и обновим файл mirrorlist"
+#echo 'Загрузка свежего списка зеркал со страницы Mirror Status, и обновим файл mirrorlist'
+# Loading a fresh list of mirrors from the Mirror Status page, and updating the mirrorlist file
+# Чтобы увидеть список всех доступных опций, наберите:
+#reflector --help
+# Команда отфильтрует пять зеркал, отсортирует их по скорости и обновит файл mirrorlist:
+#reflector --verbose --country 'Russia' -l 5 -p https -p http -n 5 --sort rate --save /etc/pacman.d/mirrorlist
+#reflector -c "Russia" -c "Belarus" -c "Ukraine" -c "Poland" -f 5 -l 5 -p https -p http -n 5 --save /etc/pacman.d/mirrorlist --sort rate
+
+#echo 'Выбор серверов-зеркал для загрузки.'
+#echo 'The choice of mirrors to download.'
+#pacman -Sy --noconfirm --noprogressbar --quiet reflector
+#reflector --verbose --country Kazakhstan --country Russia --sort rate --save /etc/pacman.d/mirrorlist
+#reflector -c "Russia" -c "Belarus" -c "Ukraine" -c "Poland" -f 20 -l 20 -p https -p http -n 20 --save /etc/pacman.d/mirrorlist --sort rate
+#Команда отфильтрует 12 зеркал russia, отсортирует по скорости и обновит файл mirrorlist
+#sudo reflector -c "Russia" -f 12 -l 12 --verbose --save /etc/pacman.d/mirrorlist
+#------------------------------------------------------------------------------
+# Reflector — скрипт, который автоматизирует процесс настройки зеркал, включающий в себя загрузку свежего списка зеркал со страницы Mirror Status.
+# https://www.linuxsecrets.com/archlinux-wiki/wiki.archlinux.org/index.php/Reflector_(%D0%A0%D1%2583%D1%2581%D1%2581%D0%BA%D0%B8%D0%B9).html
+# Эта страница сообщает о состоянии всех известных, общедоступных и активных зеркал Arch Linux:
+# https://www.archlinux.org/mirrors/status/
+# ============================================================================
+# Если возникли проблемы с обновлением, или установкой пакетов 
+# Выполните данные рекомендации:
+# author:
+#echo 'Обновление ключей системы'
+# Updating of keys of a system
+#{
+#echo "Создаётся генерация мастер-ключа (брелка) pacman, введите пароль (не отображается)..."
+#sudo pacman-key --init
+#echo "Далее идёт поиск ключей..."
+#sudo pacman-key --populate archlinux
+#echo "Обновление ключей..."
+#sudo pacman-key --refresh-keys
+#echo "Обновление баз данных пакетов..."
+#sudo pacman -Sy
+#}
+#sleep 1
+#
+# Или:
+#sudo pacman-key --init && sudo pacman-key --populate archlinux && sudo pacman-key --refresh-keys && sudo pacman -Sy
+# ============================================================================
+
+echo -e "${BLUE}:: ${NC}Обновим базы данных пакетов" 
+#echo 'Обновим базы данных пакетов'
+# Updating the package databases
+#sudo pacman-key --init
+#sudo pacman-key --refresh-keys
+sudo pacman -Sy  
+
+#----------------------------------------------------------------------------
+# Знакомьтесь, pacman - лучший пакетный менеджер в мире линукса!
+#pacman -Syy   - обновление баз пакмэна(как apt-get update в дэбианоподбных)
+#pacman -Syyu  - обновление баз плюс обновление пакетов
+# ============================================================================
+
 echo -e "${YELLOW}==> ${NC}Создадим папку (downloads), и переходим в созданную папку"
 #echo 'Создадим папку (downloads), и переходим в созданную папку'
 # Create a folder (downloads), and go to the created folder
