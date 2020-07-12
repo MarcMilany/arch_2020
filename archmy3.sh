c859b83271e108299a9d8defbd2da8e367fba9bb
MarcMilany <alecs1874@gmail.com>
2020-07-12 18:15:14 +0300

    Update archmy3.sh

--- archmy3.sh
+++ archmy3.sh
@@ -75,6 +75,18 @@ BLUE="\e[1;34m"; CYAN="\e[1;36m"; BOLD="\e[1;37m"; MAGENTA="\e[1;35m"; NC="\e[0m
 # DGRAY='\e[1;30m'  LGREEN='\e[1;32m' LBLUE='\e[1;34m'  LCYAN='\e[1;36m'    NC='\e[0m' # No Color
 # Индивидуальные настройки подсветки синтаксиса для каждого пользователя можно настраивать в конфигурационном файле /home/$USER/.bashrc
 
+#----------------------------------------------------------------------------
+
+### Automatic error detection (Автоматическое обнаружение ошибок)
+_set() {
+    set [--abefhkmnptuvxBCHP] [-o option] [arg ...]
+}
+
+_set() {
+    set -e "\n${RED}Error: ${YELLOW}${*}${NC}"
+    _note "${MSG_ERROR}"
+    sleep 1; $$
+}
 
 # Checking personal setting (Проверяйте ваши персональные настройки)
 ### Display user entries (Отображение пользовательских записей ) 
