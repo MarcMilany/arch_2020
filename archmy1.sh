2dbe82a7cc6ff6ae494a3f2ac151a4f24ac8744d
MarcMilany <alecs1874@gmail.com>
2020-07-18 13:14:34 +0300

    Update archmy1.sh

--- archmy1.sh
+++ archmy1.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+apptitle="Arch Linux Fast Install v1.6 LegasyBIOS - Version: 2020.07.16.00.40.38 (GPLv3)"
+baseurl=https://raw.githubusercontent.com/MarcMilany/arch_2020/master/url%20links%20abbreviated/git%20url
+cpl=0
+skipfont="0"
+fspkgs=""
+#
 # ============================================================================
 #######
 #echo "#################################################################"
