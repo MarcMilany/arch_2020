ca94d41b75b2b27952376718c2d291cfb715c071
MarcMilany <alecs1874@gmail.com>
2021-05-05 00:04:17 -0400

    Update arch2020.sh

--- arch2020.sh
+++ arch2020.sh
@@ -444,9 +444,9 @@ echo -e "${BLUE}:: ${NC}Arch Linux, Base devel (AUR only)"
 echo " Сценарий pacstrap устанавливает (base) базовую систему. Для сборки пакетов из AUR (Arch User Repository) также требуется группа base-devel. "
 echo -e "${MAGENTA}=> ${BOLD}Т.е., Если нужен AUR, ставь base и base-devel, если нет, то ставь только base. ${NC}"
 #pacstrap /mnt base base-devel nano dhcpcd netctl which inetutils  #wget vim
-pacman -S pkgfile  # Обозреватель метаданных pacman .files
-pacman -S extra/arch-install-scripts  # Скрипты для помощи в установке Arch Linux
-pkgfile --update  # Синхронизировать базу данных pkgfile
+pacman -S pkgfile --noconfirm  # Обозреватель метаданных pacman .files
+pacman -S extra/arch-install-scripts --noconfirm  # Скрипты для помощи в установке Arch Linux
+# pkgfile --update  # Синхронизировать базу данных pkgfile
 pkgfile pacstrap  # Найти пакет, которому принадлежит файл - pacstrap
 pacstrap -i /mnt base base-devel nano dhcpcd netctl which inetutils --noconfirm
 clear
