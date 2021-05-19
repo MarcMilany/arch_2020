536bf7832db4c6236b2fe09e05aeea094718e586
MarcMilany <alecs1874@gmail.com>
2021-05-19 14:09:04 -0400

    Update archmy2.sh

--- archmy2.sh
+++ archmy2.sh
@@ -236,9 +236,9 @@ sed -i "s/autodetect modconf/autodetect keyboard keymap modconf/g" /etc/mkinitcp
 echo "sed -i \"s/block filesystems/block encrypt lvm2 filesystems/g\" /etc/mkinitcpio.conf"
 sed -i "s/block filesystems/block encrypt lvm2 filesystems/g" /etc/mkinitcpio.conf
 echo "sed -i \"s/filesystems keyboard fsck/filesystems fsck/g\" /etc/mkinitcpio.conf"
-sed -i "s/block filesystems/block encrypt filesystems/g" /etc/mkinitcpio.conf
+sed -i "s/filesystems keyboard fsck/filesystems fsck/g" /etc/mkinitcpio.conf
 echo "sed -i \"s/#COMPRESSION="lz4"/COMPRESSION="lz4"/g\" /etc/mkinitcpio.conf"
-sed -i "s/#COMPRESSION="lz4"/COMPRESSION="lz4"/g" /etc/mkinitcpio.conf
+sed -i "s/#COMPRESSION="lz4"/COMPRESSION="lz4"/" /etc/mkinitcpio.conf
 sleep 1   
 nano /etc/mkinitcpio.conf
 # nano -m /etc/mkinitcpio.conf  # -m - включить поддержку мыши
