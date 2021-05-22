7e2d92f3cc5fb491930bc4830fc1221eb734a68f
MarcMilany <alecs1874@gmail.com>
2021-05-22 06:11:58 -0400

    Update archmy2.sh

--- archmy2.sh
+++ archmy2.sh
@@ -1027,4 +1027,63 @@ exit
 
 
 
+### Urls (current branch, gh-pages, AUR Helper, mirrors, grub theme, ipinfo)
+export URL="https://raw.githubusercontent.com/${AUTHOR}/${APPNAME}/${BRANCH}"
+export GHPAGES=\
+"https://raw.githubusercontent.com/${AUTHOR}/${APPNAME}/gh-pages"
+
+_install_bootloader() {
+
+ ### Set options
+        if [[ ${LUKS} ]]; then
+            echo "options cryptdevice=UUID=${ROOT_UUID}:cryptlvm \
+root=/dev/mapper/cryptroot quiet rw" >> "${BOOTCONF}"
+        else
+            echo "options root=UUID=${ROOT_UUID} quiet rw" >> "${BOOTCONF}"
+        fi
+        echo "-> ${MSG_DONE}"
+
+        # Update systemd-boot
+        _info "${MSG_SYSTEMDBOOT_UPDATE}"
+        _chroot "bootctl --path=/boot update"
+        echo "-> ${MSG_DONE}"
+
+    ### BIOS Firmware > Grub
+    else
+
+        # Install Grub
+        _info "${MSG_INSTALL_GRUB} ${DRIVE}"
+        INSTALL_GRUB=("${GRUB}")
+        if [[ ${NTFS} ]]; then INSTALL_GRUB+=("${GRUB_EXTRAS}"); fi
+        _chroot "pacman --noconfirm --needed -S ${INSTALL_GRUB[*]}"
+        _chroot "grub-install --target=i386-pc ${DRIVE}"
+        echo "-> ${MSG_DONE}"
+
+        # Download Themes
+        _info "${MSG_DL_GRUB_THEME}"
+        _wget "${GHPAGES}/assets/images/background.png" && echo
+        _chroot "git clone https://github.com/Generator/Grub2-themes.git"
+        echo "-> ${MSG_DONE}"
+
+        # Install Themes
+        _info "${MSG_INSTALL_GRUB_THEME}"
+        _chroot "cp -r Grub2-themes/{Archlinux,Archxion} /boot/grub/themes/"
+        sed -i -e "s/GRUB_GFXMODE=auto/GRUB_GFXMODE=1024x768/g" \
+/mnt/etc/default/grub
+        sed -i -e "s/#GRUB_THEME/GRUB_THEME/g" /mnt/etc/default/grub
+sed -i -e "s|/path/to/gfxtheme|/boot/grub/themes/Archlinux/theme.txt|g" \
+/mnt/etc/default/grub
+        echo "desktop-image: \"background.png\"" >> \
+/mnt/boot/grub/themes/Archlinux/theme.txt
+        mv background.png /mnt/boot/grub/themes/Archlinux/background.png
+        rm -rf /mnt/Grub2-themes
+        echo "-> ${MSG_DONE}"
+
+        # Configure Grub
+        _info "${MSG_SET_GRUB}"
+        _chroot "grub-mkconfig -o /boot/grub/grub.cfg"
+    fi
+}
+
+
 
